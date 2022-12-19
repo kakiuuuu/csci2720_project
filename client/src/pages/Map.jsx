39928ee0bde4db47bdf81949217049e5b3c5db94
kakiufong <fongkakiu2001@gmail.com>
2022-12-19 15:06:17 +0800

    map data update

--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -2,75 +2,21 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "../components/navbar";
 import MapWidget from "../components/MapWidget"
-function initMap(){}
+import _ from 'lodash'
+
 const Map = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const venues = useSelector((state) => state.venues);
   const { username } = useSelector((state) => state.user);
-  const locations = [
-    {
-      name: "Location 1",
-      location: { 
-        lat: 22.29386,
-        lng: 114.17053
-      },
-    },
-    {
-      name: "Location 2",
-      location: { 
-        lat: 22.44152,
-        lng: 114.02289
-      },
-    },
-    {
-      name: "Location 3",
-      location: { 
-        lat: 22.35665,
-        lng: 114.12623
-      },
-    },
-    {
-      name: "Location 4",
-      location: { 
-        lat: 22.501639,
-        lng: 114.128911
-      },
-    },
-    {
-      name: "Location 5",
-      location: { 
-        lat: 22.31368,
-        lng: 114.18556
-      },
-    },
-    {
-      name: "Location 6",
-      location: { 
-        lat: 22.334583,
-        lng: 114.208766
-      },
-    },
-    {
-      name: "Location 7",
-      location: { 
-        lat: 22.38136,
-        lng: 114.1899
-      },
-    },
-    {
-      name: "Location 8",
-      location: { 
-        lat: 22.28602,
-        lng: 114.14967
-      },
-    },
-    {
-      name: "Location 9",
-      location: { 
-        lat: 22.39181,
-        lng: 113.976771
-      },
-    },
-  ];
+
+  const locations = _.map(venues, (venue)=> {
+    let {name, latitude, longitude} = venue
+    return {
+      name,
+      location: {lat: latitude, lng: longitude}
+    }
+  })
+  
   return (
     <Box>
       <Navbar />
